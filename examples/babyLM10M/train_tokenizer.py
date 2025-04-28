64ee28ba285d9c2ac79299975446e82dc52da8be
Wagtail <Bachstelze@users.noreply.github.com>
2025-04-28 16:17:20 +0200

    Update train_tokenizer.py

--- examples/babyLM10M/train_tokenizer.py
+++ examples/babyLM10M/train_tokenizer.py
@@ -24,7 +24,7 @@ for i in range(256):
 
 #vocab_size = 32768
 # test small vocab size
-vocab_size = 4096
+vocab_size = 2048
 
 # Taken from Llama
 pat_str = r"(?i:'s|'t|'re|'ve|'m|'ll|'d)|[^\r\n\p{L}\p{N}]?\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]+[\r\n]*|\s*[\r\n]+|\s+(?!\S)|\s+"
